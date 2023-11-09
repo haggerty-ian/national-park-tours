3e7d54d3ce88b613e07f6a7143607b1b6c11ed94
Ian Haggerty <ijhaggerty@sep.com>
2023-11-09 12:29:37 -0500

    Add monitor for user-submitted info

--- emailserver/emailserver/models.py
+++ emailserver/emailserver/models.py
@@ -14,3 +14,15 @@ class Tour(models.Model):
 
     def __str__(self):
         return self.name
+
+# class TimeSlot(models.Model):
+#     time = models.DateTimeField()
+#     tour = models.ForeignKey(Tour, on_delete=models.CASCADE)
+#     facility = models.ForeignKey(Facility, on_delete=models.CASCADE)
+
+class Monitor(models.Model):
+    email = models.EmailField()
+    start_date = models.DateField(blank=True)
+    end_date = models.DateField(blank=True)
+    tour = models.ForeignKey(Tour, on_delete=models.CASCADE)
+    facility = models.ForeignKey(Facility, on_delete=models.CASCADE)
