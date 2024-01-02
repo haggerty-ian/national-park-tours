e9f3bf266c43f56f481d800f2f39ca09e2624506
Ian Haggerty <ijhaggerty@sep.com>
2024-01-02 14:28:58 -0500

    Move command functionality to helper class

--- emailserver/emailserver/management/commands/fetchfacilities.py
+++ emailserver/emailserver/management/commands/fetchfacilities.py
@@ -1,31 +1,12 @@
 from django.core.management.base import BaseCommand
-import requests
-from emailserver.models import Facility
+from emailserver.helpers.fetchfacilitieshelper import FetchFacilities
 
 class Command(BaseCommand):
     help = "Fetches all facilities as defined by the NPS"
+    helper = FetchFacilities()
 
     def add_arguments(self, parser):
         pass
 
     def handle(self, *args, **options):
-        params = {
-            "exact": "false",
-            "size": "500",
-            "fq": ["entity_type:(tour OR timedentry_tour)", "entity_type:ticketfacility"]
-        }
-        headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/111.0.0.0 Safari/537.36'}
-
-        response = requests.get("https://www.recreation.gov/api/search", params=params, headers=headers)
-        for facility in response.json()['results']:
-            if len(Facility.objects.filter(name=facility['name'])) != 0:
-                continue
-
-            matching_id_facilities = Facility.objects.filter(external_reference_id=int(facility['entity_id']))
-            if len(matching_id_facilities) != 0:
-                old_facility = matching_id_facilities.first()
-                print(f'faciliy {old_facility.external_reference_id} changed names from {old_facility.name} to {facility["name"]}')
-                old_facility.delete()
-
-            facility_model = Facility(name=facility['name'], external_reference_id=int(facility["entity_id"]))
-            facility_model.save()
+        self.helper.get_facilities()
